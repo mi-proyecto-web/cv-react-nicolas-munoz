8bbbcf6553f2e22775d0bdedd757d5bff66addac
nicolasnunosh <nicomunosh@gmail.com>
2025-11-20 16:24:24 -0500

    feat: componente CabeceraCV con datos personales actualizados

---
+++ src/components/CabeceraCV.jsx
@@ -0,0 +1,16 @@
+function CabeceraCV() {
+  return (
+    <header>
+      <h1>Nicolás Muñoz</h1>
+      <h2>Desarrollador Front-End Jr.</h2>
+
+      <ul>
+        <li>Teléfono: 3215600665</li>
+        <li>Email: nicomunosh@gmail.com</li>
+        <li>Ciudad: Medellin, Colombia</li>
+      </ul>
+    </header>
+  );
+}
+
+export default CabeceraCV;
