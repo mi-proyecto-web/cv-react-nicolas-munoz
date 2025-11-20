bd5dcafcdaa8f3cf69791cd1b85523b07abc42cd
nicolasnunosh <nicomunosh@gmail.com>
2025-11-20 16:46:33 -0500

    feat: componente Perfil con descripción personalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import CabeceraCV from "./components/CabeceraCV";
+import Perfil from "./components/Perfil";
 import "./App.css";
 
 function App() {
   return (
     <>
       <CabeceraCV />
+      <Perfil />
     </>
   );
 }
---
+++ src/components/Perfil.jsx
@@ -0,0 +1,22 @@
+function Perfil() {
+  return (
+    <section>
+      <h2>Perfil Profesional</h2>
+      <p>
+        Soy un aprendiz del Sena en analisis y desarrollo de software con
+        habilidades en JavaScript, React y bases de datos. Me destaco por mi
+        disciplina y capacidad de aprender rápidamente nuevas tecnologías.
+      </p>
+
+      <h3>Habilidades destacadas</h3>
+      <ul>
+        <li>Trabajo en equipo</li>
+        <li>Comunicación</li>
+        <li>Pensamiento lógico</li>
+        <li>Adaptabilidad</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Perfil;
