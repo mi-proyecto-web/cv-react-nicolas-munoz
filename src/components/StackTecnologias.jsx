c17be3c48e6f09d03bd50596ff8a8d88535ac104
nicolasnunosh <nicomunosh@gmail.com>
2025-11-21 02:35:25 -0500

    docs: actualización del README con EV07 y EV08

--- src/components/StackTecnologias.jsx
+++ src/components/StackTecnologias.jsx
@@ -1,51 +1,25 @@
-function StackTecnologias() {
-  const tecnologias = [
-    "JavaScript",
-    "React",
-    "Node.js",
-    "CSS",
-    "HTML",
-    "Git",
-    "GitHub",
-    "Vite",
-    "SQL",
-    "MongoDB",
-  ];
-
-  const getColor = (tech) => {
-    if (tech === "JavaScript") return "yellow";
-    if (tech === "React") return "cyan";
-    if (tech === "Node.js") return "lightgreen";
-    if (tech === "CSS") return "lightblue";
-    if (tech === "HTML") return "orange";
-    if (tech === "Git") return "salmon";
-    if (tech === "GitHub") return "gray";
-    if (tech === "Vite") return "purple";
-    if (tech === "SQL") return "royalblue";
-    return "white";
-  };
-
+function StackTecnologias({ lista }) {
   return (
-    <section>
-      <h2>Stack de Tecnologías (Dinámico + Condicional)</h2>
+    <div>
+      <h2>Stack de Tecnologías</h2>
 
       <div style={{ display: "flex", gap: "10px", flexWrap: "wrap" }}>
-        {tecnologias.map((tech, index) => (
+        {lista.map((tecno, index) => (
           <span
             key={index}
             style={{
-              padding: "8px 12px",
+              padding: "6px 12px",
               borderRadius: "8px",
-              backgroundColor: getColor(tech),
-              color: "black",
-              fontWeight: "bold",
+              backgroundColor: "#282c34",
+              color: "white",
+              fontSize: "14px",
             }}
           >
-            {tech}
+            {tecno}
           </span>
         ))}
       </div>
-    </section>
+    </div>
   );
 }
 
